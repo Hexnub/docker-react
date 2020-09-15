bd2850444a498b1f452fca231c2a303bb917f041
hexnub <ultimatetinkerer@gmail.com>
2020-09-14 21:38:00 -0700

    changed app.js text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          Hexnub's new interactive website is under construction!
         </p>
         <a
           className="App-link"
